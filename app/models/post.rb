2dcb0a3835534af7607d8783196d7dba8b71fcd7
Vladislav Kizyuk <vl.kizyuk@gmail.com>
2025-07-05 12:24:05 +0000

    fix multiline posts, assset pipeline logic

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,8 @@ class Post < ApplicationRecord
     belongs_to :user, class_name: "User", foreign_key: "author_id"
     has_many :comments, dependent: :destroy
 
-    validates :title, length: { minimum: 5 }
-    validates :body, length: { minimum: 15 }
+    validates :title, length: { minimum: 5, maximum: 50 }
+    validates :body, length: { minimum: 15, maximum: 500 }
 
     scope :random, -> { order("RANDOM()") }
     scope :from_user, ->(user_id) { where(author_id: user_id) }
