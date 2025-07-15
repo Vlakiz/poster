6774474a435f1096f7b65df5ed7492da2ef0ecc0
Vladislav Kizyuk <vl.kizyuk@gmail.com>
2025-07-16 01:45:05 +0300

    new seeds logic, fix likes association in post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post < ApplicationRecord
     belongs_to :user, class_name: "User", foreign_key: "author_id"
     has_many :comments, dependent: :destroy
-    has_many :likes, dependent: :destroy
+    has_many :likes, as: :likable, dependent: :destroy
     has_many :liking_users, through: :likes, source: :user
 
     validates :title, length: { minimum: 5, maximum: 50 }
-    validates :body, length: { minimum: 15, maximum: 500 }
+    validates :body, length: { minimum: 15, maximum: 1000 }
 
     scope :random, -> { order("RANDOM()") }
     scope :from_user, ->(user_id) { where(author_id: user_id) }
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,12 @@
 #   end
 
 puts "Cleaning database..."
+Comment.destroy_all
+Post.destroy_all
 User.destroy_all
 if Rails.env.development?
     ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name = 'users'")
 end
-Comment.destroy_all
-Post.destroy_all
 
 
 puts "Creating users..."
@@ -22,13 +22,18 @@ puts "Creating users..."
     password = Faker::Internet.password(min_length: 8)
     user_attributes = {
         email: Faker::Internet.unique.email,
-        nickname: Faker::Internet.unique.username(specifier: 7..25),
+        nickname: Faker::Internet.unique.username(specifier: 7..25, separators: ['_']),
         password: password,
-        password_confirmation: password
+        password_confirmation: password,
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        date_of_birth: Faker::Date.birthday(min_age: 16, max_age: 65),
+        registration_date: Faker::Date.backward(days: 365),
+        country: Faker::Address.country_code,
     }
     user = User.new(user_attributes)
     if user.save
-        puts "User created: #{user.nickname}"
+        puts "User created: #{user.nickname}:#{password}"
     else
         puts "Error creating user: #{user.errors.full_messages.join(', ')}"
     end
@@ -36,16 +41,26 @@ end
 
 
 puts "Creating posts..."
-10.times do |i|
+30.times do |i|
     post_attributes = {
         title: Faker::Book.title,
-        body: Faker::Lorem.paragraphs(number: 10).join("\n\n"),
-        author_id: 1 + (rand() * 10).floor,
+        body: Faker::Lorem.paragraphs(number: rand(1..15)).join("\n\n"),
+        author_id: rand(0...10),
         published_at: i.days.from_now
     }
     post = Post.new(post_attributes)
     if post.save
-        puts "Post created: #{post.title}"
+        comments_count = rand(0..7)
+        comments_count.times do
+            comment_attributes = {
+                body: Faker::Quote.jack_handey,
+                post_id: post.id,
+                user_id: rand(0...10)
+            }
+            comment = Comment.new(comment_attributes)
+            comment.save
+        end
+        puts "Post created: #{post.title} along with #{comments_count} comments"
     else
         puts "Error creating post: #{post.errors.full_messages.join(', ')}"
     end
